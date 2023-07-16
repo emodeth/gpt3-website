654c006ab309799243796c5adc86c22f6ed5926b
emodeth <emodethistaken@gmail.com>
2023-07-16 19:11:29 +0300

    Brands section added

--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -1,7 +1,22 @@
 import "./brands.scss";
+import atlasian from "../../assets/atlassian.png";
+import google from "../../assets/google.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+import slack from "../../assets/slack.png";
 
 function Brands() {
-  return <div></div>;
+  return (
+    <div className="brands">
+      <div className="brands__container">
+        <img src={google} alt="google" />
+        <img src={slack} alt="slack" />
+        <img src={atlasian} alt="atlasian" />
+        <img src={dropbox} alt="dropbox" />
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 }
 
 export default Brands;

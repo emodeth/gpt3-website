28992b104c0ca4189e6c252781606677331f1c05
emodeth <emodethistaken@gmail.com>
2023-07-16 18:18:33 +0300

    Header section added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,40 @@
 import "./header.scss";
+import imgAI from "../../assets/ai.png";
+import imgPeople from "../../assets/people.png";
 
 function Header() {
-  return <div></div>;
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__content">
+          <h1 className="header__header">
+            Let's Build Something amazing with GPT-3 OpenAI
+          </h1>
+          <p className="header__description">
+            Yet bed any for travelling assistance indulgence unpleasing. Not
+            thoughts all exercise blessing. Indulgence way everything joy
+            alteration boisterous the attachment. Party we years to order allow
+            asked of.
+          </p>
+          <form className="header__form">
+            <input className="header__input" placeholder="Your Email Address" />
+            <button className="header__button">Get Started</button>
+          </form>
+          <div className="header__people">
+            <img
+              className="header__people-image"
+              src={imgPeople}
+              alt="people"
+            />
+            <p className="header__request">
+              1,600 people requested access a visit in last 24 hours
+            </p>
+          </div>
+        </div>
+        <img className="header__ai-image" src={imgAI} alt="ai" />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
